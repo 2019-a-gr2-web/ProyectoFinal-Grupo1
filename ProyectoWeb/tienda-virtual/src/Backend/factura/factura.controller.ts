7d83eea341dcca38f0d115c2c91d9c8881fa5559
DanyJos3 <jose196.jm@hotmail.com>
2019-07-28 17:53:28 -0500

    - Eliminar Factura

--- ProyectoWeb/tienda-virtual/src/Backend/factura/factura.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('factura')
-export class FacturaController {}
--- ProyectoWeb/tienda-virtual/src/Backend/factura/factura.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FacturaService } from './factura.service';
-
-@Module({
-  providers: [FacturaService]
-})
-export class FacturaModule {}
